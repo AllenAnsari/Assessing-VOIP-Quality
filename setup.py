dacc525bffc0d496456685d22ce5371539781b4e
Allen Ansari <57706348+AllenAnsari@users.noreply.github.com>
2021-02-21 16:44:07 -0600

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mean Opinion Score (MOS) is one of the common metrics to measure audio quality. In other words MOS is the average of call quality ratings given by different users when a VOIP call ended. ',
+    author='Allen Ansari',
+    license='',
+)
